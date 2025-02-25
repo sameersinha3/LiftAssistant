b81015490f08e07c7e491ce6f89593f10b83d52b
sameersinha3 <sameer_sinha@brown.edu>
2025-02-25 17:22:19 -0600

    Initial Commit

---
+++ LiftAssistant/LiftAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftAssistantApp.swift
+//  LiftAssistant
+//
+//  Created by Sameer Sinha on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LiftAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiftAssistantTests/LiftAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftAssistantTests.swift
+//  LiftAssistantTests
+//
+//  Created by Sameer Sinha on 2/25/25.
+//
+
+import Testing
+@testable import LiftAssistant
+
+struct LiftAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
