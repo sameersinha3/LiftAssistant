6681c0cbc267e01a4bf4e76c2d9c00e7b6eff5b1
sameersinha3 <sameer_sinha@brown.edu>
2025-02-26 21:13:48 -0600

    Removed unnecessary files

--- Pods/Target Support Files/Pods-LiftAssistant-LiftAssistantUITests/Pods-LiftAssistant-LiftAssistantUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LiftAssistant_LiftAssistantUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LiftAssistant_LiftAssistantUITests
-@end
--- Pods/Target Support Files/Pods-LiftAssistant/Pods-LiftAssistant-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LiftAssistant : NSObject
-@end
-@implementation PodsDummy_Pods_LiftAssistant
-@end
